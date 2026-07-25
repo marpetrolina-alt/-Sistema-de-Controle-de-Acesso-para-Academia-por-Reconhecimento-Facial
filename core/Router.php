3c2844b484244717b036f0cdbcc58aa5b281c007
marpetrolina-alt <mar.petrolina@gmail.com>
2026-07-24 22:10:09 -0300

    Adiciona sistema de rotas MVC
    Implemented a Router class to handle GET and POST routes.

--- core/Router.php
+++ core/Router.php
@@ -1,1 +1,34 @@
+<?php
 
+class Router
+{
+    private $routes = [];
+
+    public function get($rota, $acao)
+    {
+        $this->routes['GET'][$rota] = $acao;
+    }
+
+    public function post($rota, $acao)
+    {
+        $this->routes['POST'][$rota] = $acao;
+    }
+
+    public function dispatch($metodo, $uri)
+    {
+        if (!isset($this->routes[$metodo][$uri])) {
+            http_response_code(404);
+            die("Página não encontrada.");
+        }
+
+        $acao = $this->routes[$metodo][$uri];
+
+        [$controller, $metodoController] = explode('@', $acao);
+
+        require_once "../app/Controller/$controller.php";
+
+        $objeto = new $controller();
+
+        $objeto->$metodoController();
+    }
+}
