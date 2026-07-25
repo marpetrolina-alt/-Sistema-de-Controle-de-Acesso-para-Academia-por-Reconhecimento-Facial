3a91f5c1e074e36a845b683fabdfdc9cb9c041ff
marpetrolina-alt <mar.petrolina@gmail.com>
2026-07-24 22:26:35 -0300

    Refactor router dispatch to include PDO
    Pass PDO instance to router dispatch method.

--- public/index.php
+++ public/index.php
@@ -2,7 +2,6 @@
 
 require_once "../core/Database.php";
 require_once "../core/Router.php";
-require_once "../app/Controller/AlunoController.php";
 
 $pdo = Database::conectar();
 
@@ -10,8 +9,15 @@ $router = new Router();
 
 require_once "../routes/web.php";
 
-$uri = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
+$uri = parse_url(
+    $_SERVER['REQUEST_URI'],
+    PHP_URL_PATH
+);
 
 $metodo = $_SERVER['REQUEST_METHOD'];
 
-$router->dispatch($metodo, $uri);
+$router->dispatch(
+    $metodo,
+    $uri,
+    $pdo
+);
