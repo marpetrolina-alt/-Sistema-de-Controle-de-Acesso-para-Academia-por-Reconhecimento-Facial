a85eba6baaf6e47a814c068eef51120d11f0fa13
marpetrolina-alt <mar.petrolina@gmail.com>
2026-07-24 22:28:59 -0300

    Adiciona View para edição de alunos

--- app/View/alunos/edit.php
+++ app/View/alunos/edit.php
@@ -1,1 +1,108 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
 
+<head>
+    <meta charset="UTF-8">
+
+    <title>Editar Aluno</title>
+</head>
+
+<body>
+
+    <h1>Editar Aluno</h1>
+
+    <form method="POST" action="/alunos/update">
+
+        <input
+            type="hidden"
+            name="id"
+            value="<?= $aluno['id'] ?>"
+        >
+
+        <div>
+
+            <label>Nome:</label>
+
+            <input
+                type="text"
+                name="nome"
+                value="<?= $aluno['nome'] ?>"
+                required
+            >
+
+        </div>
+
+        <br>
+
+        <div>
+
+            <label>CPF:</label>
+
+            <input
+                type="text"
+                name="cpf"
+                value="<?= $aluno['cpf'] ?>"
+                required
+            >
+
+        </div>
+
+        <br>
+
+        <div>
+
+            <label>Telefone:</label>
+
+            <input
+                type="text"
+                name="telefone"
+                value="<?= $aluno['telefone'] ?>"
+            >
+
+        </div>
+
+        <br>
+
+        <div>
+
+            <label>E-mail:</label>
+
+            <input
+                type="email"
+                name="email"
+                value="<?= $aluno['email'] ?>"
+            >
+
+        </div>
+
+        <br>
+
+        <div>
+
+            <label>Data de nascimento:</label>
+
+            <input
+                type="date"
+                name="data_nascimento"
+                value="<?= $aluno['data_nascimento'] ?>"
+            >
+
+        </div>
+
+        <br>
+
+        <button type="submit">
+            Atualizar Aluno
+        </button>
+
+    </form>
+
+    <br>
+
+    <a href="/alunos">
+        Voltar para lista
+    </a>
+
+</body>
+
+</html>
