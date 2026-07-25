59e0909971add37345190eed45febca9092d3796
marpetrolina-alt <mar.petrolina@gmail.com>
2026-07-24 22:06:55 -0300

    Adiciona conexão com banco de dados MySQL
    Implement Database class with connection method

--- core/Database.php
+++ core/Database.php
@@ -1,1 +1,35 @@
+<?php
 
+class Database
+{
+    public static function conectar()
+    {
+        $host = "localhost";
+        $dbname = "academia";
+        $user = "root";
+        $password = "";
+
+        try {
+
+            $pdo = new PDO(
+                "mysql:host=$host;dbname=$dbname;charset=utf8",
+                $user,
+                $password
+            );
+
+            $pdo->setAttribute(
+                PDO::ATTR_ERRMODE,
+                PDO::ERRMODE_EXCEPTION
+            );
+
+            return $pdo;
+
+        } catch (PDOException $e) {
+
+            die(
+                "Erro na conexão com o banco de dados: "
+                . $e->getMessage()
+            );
+        }
+    }
+}
