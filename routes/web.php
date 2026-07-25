e168ff1722e75261409fd9e5090ebba7226e4b04
marpetrolina-alt <mar.petrolina@gmail.com>
2026-07-24 22:14:26 -0300

    Adiciona rotas do CRUD de alunos

--- routes/web.php
+++ routes/web.php
@@ -1,1 +1,13 @@
+<?php
 
+$router->get('/alunos', 'AlunoController@index');
+
+$router->get('/alunos/create', 'AlunoController@create');
+
+$router->post('/alunos/store', 'AlunoController@store');
+
+$router->get('/alunos/edit', 'AlunoController@edit');
+
+$router->post('/alunos/update', 'AlunoController@update');
+
+$router->get('/alunos/delete', 'AlunoController@delete');
