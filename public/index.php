0a9891acfc1bc9b3d46ee28a4e31e9376cb40f6c
marpetrolina-alt <mar.petrolina@gmail.com>
2026-07-24 22:15:56 -0300

    Adiciona ponto de entrada da aplicação MVC

--- public/index.php
+++ public/index.php
@@ -1,1 +1,17 @@
+<?php
 
+require_once "../core/Database.php";
+require_once "../core/Router.php";
+require_once "../app/Controller/AlunoController.php";
+
+$pdo = Database::conectar();
+
+$router = new Router();
+
+require_once "../routes/web.php";
+
+$uri = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
+
+$metodo = $_SERVER['REQUEST_METHOD'];
+
+$router->dispatch($metodo, $uri);
