abb99d67cb0ae8a9bbed8d3c75ba34dcd0f7fd75
marpetrolina-alt <mar.petrolina@gmail.com>
2026-08-02 08:34:57 -0300

    Adiciona tela de login

--- app/View/auth/login.php
+++ app/View/auth/login.php
@@ -1,1 +1,60 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
 
+<head>
+
+    <meta charset="UTF-8">
+
+    <title>Login - Academia</title>
+
+</head>
+
+<body>
+
+    <h1>Login</h1>
+
+    <form method="POST" action="/login/autenticar">
+
+        <div>
+
+            <label for="email">
+                E-mail:
+            </label>
+
+            <input
+                type="email"
+                id="email"
+                name="email"
+                required
+            >
+
+        </div>
+
+        <br>
+
+        <div>
+
+            <label for="senha">
+                Senha:
+            </label>
+
+            <input
+                type="password"
+                id="senha"
+                name="senha"
+                required
+            >
+
+        </div>
+
+        <br>
+
+        <button type="submit">
+            Entrar
+        </button>
+
+    </form>
+
+</body>
+
+</html>
