d573962568581e94bbbd90434cee3c3e0670d693
marpetrolina-alt <mar.petrolina@gmail.com>
2026-08-02 09:15:26 -0300

    Implementa cookie para lembrar e-mail
    Added functionality to remember user email with cookies during authentication.

--- app/Controller/AuthController.php
+++ app/Controller/AuthController.php
@@ -20,29 +20,50 @@ class AuthController
 
     // Realizar autenticação
     public function autenticar()
-    {
-        session_start();
+{
+    session_start();
 
-        $email = $_POST['email'];
-        $senha = $_POST['senha'];
+    $email = $_POST['email'];
+    $senha = $_POST['senha'];
 
-        $usuario = $this->model->buscarPorEmail($email);
+    $usuario = $this->model->buscarPorEmail($email);
 
-        if ($usuario && password_verify($senha, $usuario['senha'])) {
+    if ($usuario && password_verify($senha, $usuario['senha'])) {
 
-            session_regenerate_id(true);
+        session_regenerate_id(true);
 
-            $_SESSION['usuario_id'] = $usuario['id'];
+        $_SESSION['usuario_id'] = $usuario['id'];
 
-            $_SESSION['usuario_nome'] = $usuario['nome'];
+        $_SESSION['usuario_nome'] = $usuario['nome'];
 
-            header("Location: /alunos");
+        // Verifica se o usuário marcou "Lembrar meu e-mail"
+        if (isset($_POST['lembrar'])) {
 
-            exit;
+            setcookie(
+                'lembrar_email',
+                $email,
+                time() + (86400 * 30),
+                '/'
+            );
+
+        } else {
+
+            // Remove o cookie caso o usuário não queira lembrar
+            setcookie(
+                'lembrar_email',
+                '',
+                time() - 3600,
+                '/'
+            );
         }
 
-        echo "E-mail ou senha inválidos.";
+        header("Location: /alunos");
+
+        exit;
     }
+
+    echo "E-mail ou senha inválidos.";
+}
 
     // Encerrar sessão
     public function logout()
