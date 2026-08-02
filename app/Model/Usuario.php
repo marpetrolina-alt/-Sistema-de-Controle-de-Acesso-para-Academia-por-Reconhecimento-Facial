cee5e800b41e5051283b44f80dd41335ecac6e6c
marpetrolina-alt <mar.petrolina@gmail.com>
2026-08-02 08:33:32 -0300

    Adiciona Model de Usuario para autenticação

--- app/Model/Usuario.php
+++ app/Model/Usuario.php
@@ -1,1 +1,44 @@
+<?php
 
+class Usuario
+{
+    private $pdo;
+
+    public function __construct($pdo)
+    {
+        $this->pdo = $pdo;
+    }
+
+    // Buscar usuário pelo e-mail
+    public function buscarPorEmail($email)
+    {
+        $sql = $this->pdo->prepare(
+            "SELECT * FROM usuarios WHERE email = ?"
+        );
+
+        $sql->execute([$email]);
+
+        return $sql->fetch(PDO::FETCH_ASSOC);
+    }
+
+    // Cadastrar novo usuário
+    public function cadastrar($dados)
+    {
+        $senhaHash = password_hash(
+            $dados['senha'],
+            PASSWORD_DEFAULT
+        );
+
+        $sql = $this->pdo->prepare("
+            INSERT INTO usuarios
+            (nome, email, senha)
+            VALUES (?, ?, ?)
+        ");
+
+        return $sql->execute([
+            $dados['nome'],
+            $dados['email'],
+            $senhaHash
+        ]);
+    }
+}
