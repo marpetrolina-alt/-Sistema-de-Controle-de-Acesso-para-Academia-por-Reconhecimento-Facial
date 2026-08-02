ea5fc1e3bd5e1d468cbace037d3e1eb2d99a5057
marpetrolina-alt <mar.petrolina@gmail.com>
2026-08-02 11:22:20 -0300

    Cria Model de Usuario

--- app/Models/Usuario.php
+++ app/Models/Usuario.php
@@ -1,1 +1,28 @@
+<?php
 
+class Usuario
+{
+    private $pdo;
+
+    public function __construct($pdo)
+    {
+        $this->pdo = $pdo;
+    }
+
+    // Buscar usuário pelo e-mail
+    public function buscarPorEmail($email)
+    {
+        $sql = "SELECT * FROM usuarios WHERE email = :email";
+
+        $stmt = $this->pdo->prepare($sql);
+
+        $stmt->bindValue(
+            ':email',
+            $email
+        );
+
+        $stmt->execute();
+
+        return $stmt->fetch(PDO::FETCH_ASSOC);
+    }
+}
